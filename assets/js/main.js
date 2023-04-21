093ce22f188a09d9f2cec828f3433b2fa8417fbc
Shenrique1970 <shvribeiro@hotmail.com>
2023-04-21 15:47:41 -0300

    Iniciando modal tentativa 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ function convertPokemonToLi(pokemon) {
                 </ol>
 
                 <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                     alt="${pokemon.name}" onclick="abrirModal('v_modal')">
             </div>
         </li>
     `
@@ -45,3 +45,8 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function abrirModal(v_modal) {
+    console.log('Cheguei')
+}
+
